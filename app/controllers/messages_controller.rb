da63b9cbf66dfbf54f2e6721e2715e36d362918c
Nenad Milenkovic <milenkovicnkv@gmail.com>
2016-07-16 15:26:42 +0000

    Finish first part of aplication

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,53 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @messages = Message.all.order("created_at DESC")
+  end
+
+  def my_message  
+  end
+
+  def show
+  end
+
+  def new
+    @message = current_user.messages.build
+  end
+
+  def create
+    @message = current_user.messages.build(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      redirect_to new_message_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
+  end
+
+
+  private
+    def message_params
+      params.require(:message).permit(:title, :description)
+    end
+    def find_message
+      @message = Message.find(params[:id])
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root "messages#index"
+
+  resources :messages do
+     resources :comments
+  end
+
+  
+
+end
